1ec81cf217bb2bc2fa744415c77a352fdb9602f1
RaymondSum <97384609+RaymondSum@users.noreply.github.com>
2022-03-22 15:32:35 +0800

    Update custom.ts

--- custom.ts
+++ custom.ts
@@ -8,33 +8,26 @@ enum MyEnum {
     //% block="one"
     One,
     //% block="two"
-    Two
+    Two,
     //% block="three"
+    Three
 }
 
 /**
- * Custom blocks
+ * animation blocks
  */
-//% weight=100 color=#0fbc11 icon=""
+//% weight=80 color=#0fbc11 icon=""
 namespace animations {
     /**
      * Blink an LED 
      * TODO: describe your function here
-     * @param n describe parameter here, eg: 5
-     * @param s describe parameter here, eg: "Hello"
-     * @param e describe parameter here
+     * @param x hori. led
+     * @param y vert. led
+     * @param interval time in ms for blinks
      */
-    //% block="do stuff with $n and $s and $e"
-    export function blink(n: number, s: string, e: MyEnum): void {
+    //% block="blink with $x and $y and $interval"
+    export function blink(n: number, s: string, interval: MyEnum): void {
         // Add code here
     }
-
-    /**
-     * TODO: describe your function here
-     * @param value describe value here, eg: 5
-     */
-    //% block="compute fib with $value"
-    export function fib(value: number): number {
-        return value <= 1 ? value : fib(value -1) + fib(value - 2);
-    }
 }
+ 
