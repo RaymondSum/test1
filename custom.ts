5c588e4b846d026d2ebc97ebe4b64755107dfde1
RaymondSum <97384609+RaymondSum@users.noreply.github.com>
2022-03-22 16:54:02 +0800

    change ms

--- custom.ts
+++ custom.ts
@@ -26,8 +26,13 @@ namespace animations {
      * @param interval time in ms for blinks
      */
     //% block="blink with x $x and y $y and $interval ms"
+
     //% x.min=0 x.max=4 y.min=0 y.max=4
+    //% x.min=0 x.max=4 y.min=0 y.max=4
+
     //% x.fieldOptions.precision=1 y.fieldOptions.precision=1
+    //% interval.shadow=timePicker
+
     export function blink(x: number, y: number, interval: number): void {
         let sprite = game.createSprite(x,y)
         sprite.setBlink(interval)
